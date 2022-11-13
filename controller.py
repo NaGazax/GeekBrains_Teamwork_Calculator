eff6d3e351e5218000b973fea894dab2b29e3593
Tasha <nataza7394@gmail.com>
2022-11-13 16:34:43 +0300

    I have created a simple interface

--- controller.py
+++ controller.py
@@ -1,1 +1,10 @@
 # Все связующий и управляющий скрипт
+
+import view
+import operations
+import log
+
+def button_click():
+    value_lst = view.get_value()
+    operations.init(value_lst)
+
--- view.py
+++ view.py
@@ -1,1 +1,8 @@
 # Здесь интерфейс
+
+def get_value():
+    return input("Input you operation. Please separate numbers from the sign of operation with space: ")
+
+def output():
+    print("...")
+
